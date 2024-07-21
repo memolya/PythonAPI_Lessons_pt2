b8e4061c09c5405d26211519f0a88111cde72e41
Helga <semenishevaolchik@yandex.ru>
2024-07-21 22:43:55 +0300

    lessons

--- get_category_541.py
+++ get_category_541.py
@@ -5,8 +5,11 @@ def get_categories():
     """Получаем список категорий"""
     url_categories = 'https://api.chucknorris.io/jokes/categories'
     result_categories = requests.get(url_categories)
+    #преобразуем полученный json в list
     categories = list(result_categories.json())
+    #печатаем результат работы скрипта
     print(url_categories)
     print('Categories:', categories)
+    #возвращаем список categories для использования в основном скрипте
     return categories
 
