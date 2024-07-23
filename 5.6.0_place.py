aecaec89ed6edcf91add4928c4638bac8044db8c
Helga <semenishevaolchik@yandex.ru>
2024-07-23 21:42:21 +0300

    lessons

---
+++ 5.6.0_place.py
@@ -0,0 +1,80 @@
+import requests
+
+class Test_new_location():
+    """Работа с новой локацией - создание, изменение, удаление, информация"""
+
+    def test_create_location(self):
+        """Метод для создания локации"""
+
+        base_url = 'https://rahulshettyacademy.com' #базовый url
+        key = '?key=qaclick123'                     #параметр для всех запросов
+
+        """Создание новой локации"""
+        post_resource = '/maps/api/place/add/json'  #реурс для метода post
+
+        post_url = base_url + post_resource + key   #полная ссылка для метода post
+        print(post_url)
+
+        json_create_new_location = {
+            "location": {
+
+                "lat": -38.383494,
+
+                "lng": 33.427362
+
+            }, "accuracy": 50,
+
+            "name": "Frontline house",
+
+            "phone_number": "(+91) 983 893 3937",
+
+            "address": "29, side layout, cohen 09",
+
+            "types": [
+
+                "shoe park",
+
+                "shop"
+
+            ],
+
+            "website": "http://google.com",
+
+            "language": "French-IN"
+
+        }
+
+        result_post = requests.post(post_url, json = json_create_new_location) #в скобки помещаем ссылку и тело запроса
+        print(result_post.text)
+        print('Статус код: ' + str(result_post.status_code))
+        assert 200 == result_post.status_code
+        if result_post.status_code == 200:
+            print('Успешно: мы создали новую локацию.')
+        else:
+            print('Провал: запрос ошибочный.')
+
+        check_post = result_post.json() #получать ответ в json формате и значения определенных полей из него
+        check_info_post = check_post.get('status') #получаем значение поля status (с кодом ответа)
+        print('Статус код: ' + check_info_post)
+        assert check_info_post == 'OK'
+        print('Статус ответа верен.')
+        place_id = check_post.get('place_id')
+        print('ID места: ' + place_id)
+
+        """Проверка создания новой локации"""
+        get_resource = '/maps/api/place/get/json'     #ресурс для метода get
+        #https://rahulshettyacademy.com/maps/api/place/get/json?key=qaclick123&place_id=05e78841ef75b5a76de73e6372910dc - полная ссылка (пример)
+        place_id_key = '&place_id='                   #добавляем к ключу place id
+        get_url = base_url + get_resource + key + place_id_key + place_id
+        print(get_url)
+        result_get = requests.get(get_url)            #в get запросах нет body
+        print(result_get.text)                        #выдаем результат запроса get
+
+        assert 200 == result_post.status_code
+        if result_get.status_code == 200:
+            print('Успешно: проверка создания новой локации прошла успешно.')
+        else:
+            print('Провал: запрос ошибочный.')
+
+new_place = Test_new_location() #экземпляр класса
+new_place.test_create_location() #к экземпляру вызываем метод по созданию новой локации
